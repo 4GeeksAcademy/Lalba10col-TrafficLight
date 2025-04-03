ba161aa6092dc0641cd7cd80e01ec5c1d751b272
Luis Angel Alba Malagon <lalba10@icloud.com>
2025-04-03 05:08:18 +0000

    Avance proyecto 03042025

---
+++ src/js/Home.jsx
@@ -0,0 +1,35 @@
+//import React, { useState } from "react";
+//import '../styles/index.css';
+
+//const Home = () => {
+    //const [color, setColor] = useState("");
+
+    //return (
+        //<div className="semaforo">
+            //<div
+                //onClick={() => setColor("rojo")}
+                //className={"luz rojo" + (color === "rojo" ? " activo" : "")}>
+            //</div>
+            //<div
+                //onClick={() => setColor("amarillo")}
+                //className={"luz amarillo" + (color === "amarillo" ? " activo" : "")}>
+            //</div>
+            //<div
+                //onClick={() => setColor("verde")}
+                //className={"luz verde" + (color === "verde" ? " activo" : "")}>
+            //</div>
+        //</div>
+   // );
+//};
+
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>¡Hola, React funciona!</h1>
+        </div>
+    );
+};
+
+export default Home;
